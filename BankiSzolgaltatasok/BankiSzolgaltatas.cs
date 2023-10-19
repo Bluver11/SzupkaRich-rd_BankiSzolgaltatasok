fd55237ff8558f35c904b653a28c8b61862d743b
Petrik Diak <diak@petrik.hu>
2023-10-19 11:06:15 +0200

    A osztályok megalkotása

---
+++ BankiSzolgaltatasok/BankiSzolgaltatas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankiSzolgaltatasok
+{
+	internal abstract class BankiSzolgaltatas
+	{
+
+		private Tulajdonos tulajdonos;
+
+		protected BankiSzolgaltatas(Tulajdonos tulajdonos)
+		{
+			this.tulajdonos = tulajdonos;
+		}
+
+		internal Tulajdonos Tulajdonos { get => tulajdonos; }
+	}
+}
---
+++ BankiSzolgaltatasok/Szamla.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankiSzolgaltatasok
+{
+	public abstract class Szamla : BankiSzolgaltatas
+	{
+
+		protected int aktualisEgyenleg;
+
+		public Szamla(Tulajdonos tulajdonos,int aktualisEgyenleg):base(tulajdonos)
+		{
+			this.aktualisEgyenleg = 0;
+		}
+
+		private int AktualisEgyenleg { get => aktualisEgyenleg;}
+
+
+		public void Befizet(int osszeg)
+		{
+			this.aktualisEgyenleg += osszeg;
+		}
+
+		public abstract bool Kivesz(int osszeg);
+	}
+}
